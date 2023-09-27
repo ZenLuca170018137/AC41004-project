49d1e9f6732b7282e602af1b220df515df55bde0
ZenLuca170018137 <72395371+ZenLuca170018137@users.noreply.github.com>
2023-09-27 10:58:23 +0100

    first bucket attempt

--- 0-aws-provider.tf
+++ 0-aws-provider.tf
@@ -1,7 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
- 
+terraform {
+  backend "s3" {
+    bucket                  = "awsbucketteam8"
+    key                     = "my-terraform-project"
+    region                  = "us-east-1"
+    shared_credentials_file = "~/.aws/credentials"
+  }
+}
 terraform {
   required_providers {
     kubectl = {
