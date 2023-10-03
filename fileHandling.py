4ec2558464ae23dc01fec77675c06c40d7917042
ZenLuca170018137 <72395371+ZenLuca170018137@users.noreply.github.com>
2023-10-03 13:07:52 +0100

    fixed the python script

--- fileHandling.py
+++ fileHandling.py
@@ -19,35 +19,35 @@ def fileHandling(filename, variables):
 
     data[20] = '      cidr_block        = "' + output["private_subnets"][0] + '"' + '\n'
     data[21] = '      availability_zone = "' + output["private_subnets"][1] + '"' + '\n'
-    data[22] = '      cidr_block        = "' + output["private_subnets"][2] + '"' + '\n'
-    data[23] = '      availability_zone = "' + output["private_subnets"][3] + '"' + '\n'
-    data[24] = '      cidr_block        = "' + output["private_subnets"][4] + '"' + '\n'
-    data[25] = '      availability_zone = "' + output["private_subnets"][5] + '"' + '\n'
-    data[26] = '      cidr_block        = "' + output["private_subnets"][6] + '"' + '\n'
-    data[27] = '      availability_zone = "' + output["private_subnets"][7] + '"' + '\n'
+    data[24] = '      cidr_block        = "' + output["private_subnets"][2] + '"' + '\n'
+    data[25] = '      availability_zone = "' + output["private_subnets"][3] + '"' + '\n'
+    data[28] = '      cidr_block        = "' + output["private_subnets"][4] + '"' + '\n'
+    data[29] = '      availability_zone = "' + output["private_subnets"][5] + '"' + '\n'
+    data[33] = '      cidr_block        = "' + output["private_subnets"][6] + '"' + '\n'
+    data[34] = '      availability_zone = "' + output["private_subnets"][7] + '"' + '\n'
 
 
-    data[35] = '  default = ["' + output["namespaces"][0] + '"]' + '\n'
+    data[40] = '  default = ["' + output["namespaces"][0] + '"]' + '\n'
 
 
-    data[48] = '      from_port   = ' + str(output["ingress_HTTPS"]["from_port"])  + '\n'
-    data[49] = '      to_port     = ' + str(output["ingress_HTTPS"]["to_port"]) + '\n'
-    data[50] = '      protocol    = "' + output["ingress_HTTPS"]["protocol"] + '"' + '\n'
+    data[53] = '      from_port   = ' + str(output["ingress_HTTPS"]["from_port"])  + '\n'
+    data[54] = '      to_port     = ' + str(output["ingress_HTTPS"]["to_port"]) + '\n'
+    data[55] = '      protocol    = "' + output["ingress_HTTPS"]["protocol"] + '"' + '\n'
 
-    data[54] = '      from_port   = ' + str(output["ingress_HTTP"]["from_port"])  + '\n'
-    data[55] = '      to_port     = ' + str(output["ingress_HTTP"]["to_port"]) + '\n'
-    data[56] = '      protocol    = "' + output["ingress_HTTP"]["protocol"] + '"' + '\n'
+    data[59] = '      from_port   = ' + str(output["ingress_HTTP"]["from_port"])  + '\n'
+    data[60] = '      to_port     = ' + str(output["ingress_HTTP"]["to_port"]) + '\n'
+    data[61] = '      protocol    = "' + output["ingress_HTTP"]["protocol"] + '"' + '\n'
 
-    data[60] = '      from_port   = ' + str(output["ingress_custom"]["from_port"])  + '\n'
-    data[61] = '      to_port     = ' + str(output["ingress_custom"]["to_port"]) + '\n'
-    data[62] = '      protocol    = "' + output["ingress_custom"]["protocol"] + '"' + '\n'
+    data[65] = '      from_port   = ' + str(output["ingress_custom"]["from_port"])  + '\n'
+    data[64] = '      to_port     = ' + str(output["ingress_custom"]["to_port"]) + '\n'
+    data[63] = '      protocol    = "' + output["ingress_custom"]["protocol"] + '"' + '\n'
 
 
-    data[76] = '    desired_size   = ' + str(output["capacity"]["desired_size"]) + '\n'
-    data[77] = '    min_size       = ' + str(output["capacity"]["min_size"]) + '\n'
-    data[78] = '    max_size       = ' + str(output["capacity"]["max_size"]) + '\n'
-    data[79] = '    instance_types = ["' + str(output["capacity"]["instance_types"][0]) + '"]' + '\n'
-    data[80] = '    capacity_type  = "' + output["capacity"]["capacity_type"] + '"'+ '\n'
+    data[81] = '    desired_size   = ' + str(output["capacity"]["desired_size"]) + '\n'
+    data[82] = '    min_size       = ' + str(output["capacity"]["min_size"]) + '\n'
+    data[83] = '    max_size       = ' + str(output["capacity"]["max_size"]) + '\n'
+    data[84] = '    instance_types = ["' + str(output["capacity"]["instance_types"][0]) + '"]' + '\n'
+    data[85] = '    capacity_type  = "' + output["capacity"]["capacity_type"] + '"'+ '\n'
 
 
     with open(variables, 'w', encoding='utf-8') as file:
